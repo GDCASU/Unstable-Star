d9d5f52d0c11b22859158dc711496a87a265db98
JerryMouASU <swang497@asu.edu>
2023-10-13 12:17:56 -0700

    update controller

--- Assets/Scripts/JerryPlayerController.cs
+++ Assets/Scripts/JerryPlayerController.cs
@@ -8,14 +8,25 @@ public class JerryPlayerController : MonoBehaviour
     private Vector2 playerMovementVector;
     private Vector2 mousePosition;
     public float moveSpeed = 10f;
-    public Rigidbody2D playerRB;
-  //  public Camera camRef;
+    // public Rigidbody2D playerRB;
+    public Transform playerTF;
+
+
+    [SerializeField]private float _boundaryXMax;
+    [SerializeField] private float _boundaryXMin;
+    [SerializeField] private float _boundaryYMax;
+    [SerializeField] private float _boundaryYMin;
+
+    //  public Camera camRef;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+
+
     }
 
     // Update is called once per frame
@@ -23,7 +34,42 @@ public class JerryPlayerController : MonoBehaviour
     {
         playerMovementVector.x = Input.GetAxisRaw("Horizontal");
         playerMovementVector.y = Input.GetAxisRaw("Vertical");
-       // mousePosition = camRef.ScreenToWorldPoint(Input.mousePosition);
-        playerRB.MovePosition(playerRB.position + playerMovementVector * moveSpeed * Time.fixedDeltaTime);
+        // mousePosition = camRef.ScreenToWorldPoint(Input.mousePosition);
+
+        MovePlayer(playerMovementVector.x, playerMovementVector.y);
+
+    }
+
+
+
+
+    void MovePlayer(float x, float y)
+    {
+
+        Vector3 position = playerTF.transform.position;
+
+
+        //Out of bound Checker
+        if (position.y>= _boundaryYMax&&y>0|| position.y <= _boundaryYMin && y < 0)
+        {
+            y = 0;
+        }
+        //if (position.x >= _boundaryXMax && x > 0 || position.x <= _boundaryXMin && x < 0)
+        //{
+        //    x = 0;
+        //}
+
+
+        position += new Vector3(x * moveSpeed * Time.deltaTime, y * moveSpeed * Time.deltaTime);
+
+        if (position.x >= _boundaryXMax && x > 0) {
+            position.x = _boundaryXMin;
+        }
+        if (position.x <= _boundaryXMin && x < 0) {
+            position.x = _boundaryXMax;
+        }
+        playerTF.transform.position = position;
+
+
     }
 }
