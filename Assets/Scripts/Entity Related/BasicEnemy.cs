479546284dfc2c250e80ac97344470fd3dfc75a8
FalconKiaT <117485567+FalconKiaT@users.noreply.github.com>
2023-10-27 15:48:27 -0700

    Re-organizing folders and deleted old files
    Moved scripts around for better organization

---
+++ Assets/Scripts/Entity Related/BasicEnemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//INHERITS FROM THE "ParentEnemy" Class
+public class BasicEnemy : ParentEnemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
