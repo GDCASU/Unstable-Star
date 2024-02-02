d12f4ac29f3a6959980ed342ea80c182cc62d344
FalconKiaT <117485567+FalconKiaT@users.noreply.github.com>
2024-02-02 16:08:58 -0700

    Player Edge Screen Crossing v1
    Finished the system for the player appearing on the other side of the screen and keeping all their things.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 60.0f;
-    Vector2 movementVector;
+    [SerializeField] private GameObject playerCopyPrefab;
+    private GameObject currentCopyPlayer;
+    private bool playerCopyExists = false;
+    private Vector2 movementVector;
+    private Vector3 translationVector;
+    private Coroutine checkIfOffScreen;
 
     // Local Variables
     private Vector2 screenBounds;
-    private float objectHeight = 3;
-    private float playerModelHeight;
+    private readonly float playerModelHeight = 2;
     private Vector3 viewPos;
 
     private void Awake()
@@ -21,37 +25,90 @@ public class PlayerController : MonoBehaviour
             Camera.main.transform.position.z));
     }
 
-
-    private void Start()
-    {
-
-    }
-
     void Update()
     {
         // Update position of player
-        
         movePlayer();
-    }
-
-    private void LateUpdate()
-    {
-        viewPos = this.transform.position;
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y + playerModelHeight, screenBounds.y * -1 - playerModelHeight);
-        transform.position = viewPos;
     }
 
     void movePlayer()
     {
-        // TODO: Detect Player approaching the edge of the screen
-
-
-
-
         // Move player according to input
         movementVector = PlayerInput.instance.movementInput;
-        transform.Translate(movementVector * Time.deltaTime * speed);
-        // Restrict player movement here
-        this.transform.position = movementVector;
+        translationVector = speed * Time.deltaTime * movementVector;
+        transform.Translate(translationVector);
+
+        // Copy Movement over to copy player if they exist
+        if (currentCopyPlayer != null) { currentCopyPlayer.transform.Translate(translationVector); }
+
+        // Stop him if at the top and bottom of the screen
+        viewPos = this.transform.position;
+        float newY = Mathf.Clamp(viewPos.y, screenBounds.y + 2 * playerModelHeight, (screenBounds.y * -1) - playerModelHeight);
+        viewPos.y = newY;
+        this.transform.position = viewPos;
+
+        // Copy Position over to copy player if they exist
+        if (currentCopyPlayer != null)
+        { 
+            Vector3 copyPos = currentCopyPlayer.transform.position;
+            copyPos.y = newY;
+            currentCopyPlayer.transform.position = copyPos;
+        }
+    }
+
+    // Detect Player approaching the edge of the screen
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Screen Bounds") && !playerCopyExists)
+        {
+            // Create the copy player at the other side of the screen
+            Vector3 pos = this.transform.position;
+            Quaternion rotation = this.transform.rotation;
+            currentCopyPlayer = Instantiate(playerCopyPrefab, pos, rotation);
+            pos = currentCopyPlayer.transform.position;
+            pos.x *= -1;
+            currentCopyPlayer.transform.position = pos; // Agh unity why wont you let me edit x directly :c
+            playerCopyExists = true;
+
+            // Start Checking if the copy has went off screen
+            checkIfOffScreen = StartCoroutine(CheckWhoIsCloser());
+        }
+    }
+
+    // Detect when the copy player leaves the collider boxes and delete it
+    private void OnTriggerExit(Collider other)
+    {
+        if (currentCopyPlayer != null)
+        {
+            StopCoroutine(checkIfOffScreen);
+            Destroy(currentCopyPlayer);
+            playerCopyExists = false;
+        }
+    }
+
+    private IEnumerator CheckWhoIsCloser()
+    {
+        // The lower absolute value of x between the two objects should be the player
+        float xPosPlayer;
+        float xPosCopy;
+
+        // Run until the coroutine is stopped
+        while (true)
+        {
+            xPosPlayer = Mathf.Abs(this.gameObject.transform.position.x);
+            xPosCopy = Mathf.Abs(currentCopyPlayer.transform.position.x);
+
+            // If the copy player is closer to the center of the screen, swap the player with the copy
+            if (xPosCopy < xPosPlayer)
+            {
+                Vector3 tempPos = this.gameObject.transform.position;
+                this.gameObject.transform.position = currentCopyPlayer.transform.position;
+                currentCopyPlayer.transform.position = tempPos;
+            }
+
+            // Wait a frame
+            yield return null;
+        }
+   
     }
 }
