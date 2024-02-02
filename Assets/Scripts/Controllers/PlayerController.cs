89240590a8b05ade1865a5f06a98304ea1a6e1bd
JerryMouASU <swang497@asu.edu>
2024-02-02 15:42:21 -0700

    Update PlayerController.cs
    This should be abandon, Ian's branch have a better approach.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 60.0f;
     private float xRange = 23.5f;
-
+    public GameObject subModel;
+    public Vector3 subModelPos = new Vector3(50, 0, 0);
     Vector2 movementVector;
 
     void Update()
@@ -16,14 +17,28 @@ public class PlayerController : MonoBehaviour
 
     void movePlayer()
     {
+        if (transform.position.x >= 0)
+        {
+            subModel.transform.localPosition = -subModelPos;
+        }
+        else {
+            subModel.transform.localPosition = subModelPos;
+        }
+
+
+
+
+
         //puts player on other side of screen if touching side walls
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector2(xRange, transform.position.y);
+            
         }
         if (transform.position.x > xRange)
         {
             transform.position = new Vector2(-xRange, transform.position.y);
+           
         }
 
         movementVector = PlayerInput.instance.movementInput;
