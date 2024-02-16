27ee776b6db392c101da56fa58e4d9413b39652a
Ryan Carpenter <79438364+dev-rygy@users.noreply.github.com>
2024-02-16 15:10:21 -0700

    Starting Wave System

---
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaveManager : MonoBehaviour
+{
+    private const string WAVE_PARENT_NAME = "Waves";
+
+    public static WaveManager instance;
+
+    [Header("Wave Pools")]
+    [SerializeField] private List<WavePool> wavePools = new List<WavePool>();
+
+    [Header("Events")]
+    [SerializeField] public UnityEvent<int> onWaveStart;
+
+    private GameObject waveParent;
+    private WavePool currentWavePool = null;
+    private int waveCounter = 1;
+
+    private void Awake()        // Handle Singleton
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        // CHECK IF WAVE SELECTED IS NULL TO PREVENT NULL REF EXC
+        waveParent = GameObject.Find(WAVE_PARENT_NAME);
+
+        currentWavePool = wavePools[0];     // Start currentWavePool at first wave in list
+        SpawnWave();                        // Spawn the first wave
+    }
+
+    public void SpawnWave()
+    {
+        // Event for UI
+        onWaveStart?.Invoke(waveCounter);
+        // Spawn wave using RandomWaveSelect()
+        GameObject wave = Instantiate(currentWavePool.RandomWaveSelect(), waveParent.transform);
+
+        if (!wave)
+        {
+            // handle errors with no waves existing in a wavepool
+            Debug.Log("Error: Could not spawn wave");
+        }
+    }
+
+    public void UpdateWaveCounter()         // Update waveCounter and check if we need to raise the difficulty
+    {
+        waveCounter++;
+    }
+}
