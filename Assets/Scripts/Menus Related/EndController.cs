56b90f315f876f7d835029b6598448d8d2c1b13d
camilee03 <crlee15@asu.edu>
2024-03-05 18:31:45 -0700

    Finished Menu Related Buttons
    - Implemented a select function using arrow and tab keys
    - Implemented brightness, volume, sensitivity, and fullscreen control

---
+++ Assets/Scripts/Menus Related/EndController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndController : MonoBehaviour
+{
+    [Header("Menu Controller")]
+    public bool isEnd;
+    [SerializeField] private GameObject endObject;
+
+    [Header("Physics Variables")]
+    private float acceleration = 0.01f;
+    Vector3 minVelocity = new Vector3(0f, 0f, 0f);
+    Vector3 maxVelocity;
+    Vector3 currentVelocity;
+    bool enterScreen = true;
+
+    private void Start()
+    {
+        maxVelocity = new Vector3(0f, -acceleration * 20f, 0f);
+    }
+
+    private void Update()
+    {
+        if (isEnd)
+        {
+            endObject.SetActive(true);
+            Time.timeScale = 0f;
+            EnterScreen();
+        }
+        else
+        {
+            endObject.SetActive(false); 
+            Time.timeScale = 1f;
+        }
+
+    }
+
+    public void LoadMenu()
+    {
+        isEnd = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void RestartScene()
+    {
+        isEnd = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void EnterScreen() // method that causes ship to enter screen
+    {
+        if (maxVelocity != minVelocity && enterScreen)
+        {
+            currentVelocity = Vector3.Lerp(maxVelocity, minVelocity, acceleration);
+            endObject.transform.Translate(currentVelocity);
+            maxVelocity = currentVelocity;
+        }
+        else
+        {
+            enterScreen = false;
+            minVelocity = new Vector3(0f, 0f, 0f);
+            maxVelocity = new Vector3(0f, -.3f, 0f);
+        }
+    }
+}
--- Assets/Scripts/Menus Related/PauseMenu.cs
+++ Assets/Scripts/Menus Related/PauseMenu.cs
@@ -68,4 +68,5 @@ public class PauseMenu : MonoBehaviour
         Scene_Switch.instance.scene_changer("Menu");
     }
 
+
 }
