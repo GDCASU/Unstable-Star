7760ced6e70e627835ee88cfb99b1a0abb91696a
Shantanu <95384818+ShantanuT26@users.noreply.github.com>
2024-03-16 18:21:01 -0700

    Further Testing for the HUD

---
+++ Assets/Scripts/HUD-UI Related/TestBaseScript.cs
@@ -0,0 +1,35 @@
+using FMOD;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestBaseScript : MonoBehaviour
+{
+    public GameObject prefab;
+    public Sprite pistolSprite;
+    public Image testImage;
+    public WeaponArsenal weaponArsenalScript;
+    public SoundTag soundTag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponArsenalScript = GameObject.Find("Weapon Arsenal").GetComponent<WeaponArsenal>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Debug.Log(weaponArsenalScript.weaponArsenal[0].sName);
+        if (Input.GetKeyDown(KeyCode.M))
+        { 
+            Pistol myPistol = new Pistol(prefab, pistolSprite, soundTag, 3f, 2, "Pistol", 3f);
+            weaponArsenalScript.AddWeaponToArsenal(myPistol);
+            
+            //weaponArsenalScript.weaponArsenal[0] = new Pistol(prefab, pistolSprite, soundTag, 3f, 2, "Pistol", 3f);
+            testImage.sprite = weaponArsenalScript.weaponArsenal[0].weaponIcon;
+        }
+    }
+}
