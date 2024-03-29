a1e8b9c9999ba27fe79bb314c9581112753e6b78
Shantanu <95384818+ShantanuT26@users.noreply.github.com>
2024-03-29 11:41:41 -0700

    Update Player.cs

--- Assets/Scripts/Entity Related/Player/Player.cs
+++ Assets/Scripts/Entity Related/Player/Player.cs
@@ -112,7 +112,7 @@ public class Player : CombatEntity
     public void SwitchToNextWeapon()
     {
         WeaponArsenal.instance.SwitchToNextWeapon();
-        TestBaseScript.myInstance.Explosion();
+        TestBaseScript.myInstance.testMethodForShantanu();
     }
 
     /// <summary> Switches to the previous weapon in the arsenal </summary>
