7faf6b4945c46e0eee3546cdcb2f26b226a62803
Aaron Huggins <achuggi1@asu.edu>
2024-03-29 17:06:07 -0700

    Fix the wave pool system
    Patched up the last of the wave pool system in order to have the mapping version work correctly, and exposed the center and size to the editor for designers to edit.

--- Assets/Scripts/Wave System/Wave.cs
+++ Assets/Scripts/Wave System/Wave.cs
@@ -10,19 +10,15 @@ public class Wave : MonoBehaviour
 {
     [SerializeField] private List<GameObject> enemyList;
 
+    [SerializeField] float enemyBoundsCenter = .75f;
+    [SerializeField] float enemyBoundsSize = .15f;
+
     private float timerTime = 5f;
-
-    [SerializeField] private bool debug = false;
-
-
-    
+    private bool debug = false;
 
     private void Awake()
     {
-        
-        ResetAndPopulateEnemyList();
-
-        
+        ResetAndPopulateEnemyList();   
     }
 
     private void Start()
@@ -33,77 +29,42 @@ public class Wave : MonoBehaviour
     private void YReposition() {
 
         float average = 0;
-        int i = 0;
-        foreach (GameObject g in enemyList)
-        {
-            average += g.transform.position.y;
-            i++;
-        }
-        average = average / i;
-
-        //foreach (GameObject g in enemyList)
-        //{
-        //    Enemy e = g.GetComponent<Enemy>();
-        //     e.setYOffSet(g.transform.localPosition.yaverage);
-        //}
-
         float top = float.MinValue, bottom = float.MaxValue;
 
         foreach (var g in enemyList)
         {
+            average += g.transform.position.y;
+
             if (g.transform.position.y < bottom) bottom = g.transform.position.y;
             if (g.transform.position.y > top) top = g.transform.position.y;
         }
 
-        var yTop = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, -Camera.main.transform.position.z)).y;
-        var yBot = Camera.main.ViewportToWorldPoint(new Vector3(0, .5f, -Camera.main.transform.position.z)).y;
+        average /= enemyList.Count;
 
-        Debug.Log("BOTTOM: " + bottom);
-        Debug.Log("TOP: " + top);
-        Debug.Log("Y_BOT: " + yBot);
-        Debug.Log("T_TOP: " + yTop);
+        var yTop = Camera.main.ViewportToWorldPoint(new Vector3(0, enemyBoundsCenter + enemyBoundsSize, -Camera.main.transform.position.z)).y;
+        var yBot = Camera.main.ViewportToWorldPoint(new Vector3(0, enemyBoundsCenter - enemyBoundsSize, -Camera.main.transform.position.z)).y;
 
-        // needs clamping
-        if (Camera.main.ViewportToWorldPoint(new Vector3(0, .9f, -Camera.main.transform.position.z)).y < (top - bottom))
+        if (debug)
         {
-            Debug.Log("AAA");
-            // actual - bottom / top - bottom
-            foreach (var g in enemyList)
-            {
-                var actual = g.transform.position.y;
-
-
-                var percentage = (actual - bottom) / (top - bottom);
-
-
-
-
-                var toPos = Mathf.Lerp(-.2f, .2f, percentage);
-
-
-     
-                g.GetComponent<Enemy>().setArrivalPercentUpScreen(1f);
-                Debug.Log(Camera.main.ViewportToWorldPoint(new Vector3(0, 1, -Camera.main.transform.position.z)));
-            }
-        }
-        else {
-           // Debug.Log("BBB");
-            foreach (var g in enemyList)
-            {
-                var actual = g.transform.position.y;
-                //  var percentage = ( actual-bottom) / (Camera.main.ViewportToWorldPoint(new Vector3(0, .75f, -Camera.main.transform.position.z)).y - Camera.main.ViewportToWorldPoint(new Vector3(0, .6f, -Camera.main.transform.position.z)).y);
-                var percentage = (actual - average) / Camera.main.ViewportToWorldPoint(new Vector3(0,1f, -Camera.main.transform.position.z)).y +.5f;
-                  var toPos = Mathf.Lerp(-.15f, .15f, percentage) 
-                    ;
-
-          
-                g.GetComponent<Enemy>().setArrivalPercentUpScreen(.75f + toPos);
-            }
+            Debug.Log("BOTTOM: " + bottom);
+            Debug.Log("TOP: " + top);
+            Debug.Log("Y_BOT: " + yBot);
+            Debug.Log("T_TOP: " + yTop);
         }
 
+        // needs mapping
+        foreach(var g in enemyList)
+        {
+            var actual = g.transform.position.y;
+
+            float percentage;
+            if(yTop - yBot < top - bottom) percentage = (actual - bottom) / (top - bottom); // Needs mapping
+            else percentage = (actual - average) / Camera.main.ViewportToWorldPoint(new Vector3(0, 1f, -Camera.main.transform.position.z)).y + .5f;
+
+            var toPos = Mathf.Lerp(-enemyBoundsSize, enemyBoundsSize, percentage);
+            g.GetComponent<Enemy>().setArrivalPercentUpScreen(enemyBoundsCenter + toPos);
+        }
     }
-
-
 
     private bool CheckListEmpty()
     {
@@ -130,6 +91,11 @@ public class Wave : MonoBehaviour
         }
     }
 
+    public void SetBoundFields(float enemyBoundsCenter, float enemyBoundsSize)
+    {
+        this.enemyBoundsCenter = enemyBoundsCenter;
+        this.enemyBoundsSize = enemyBoundsSize;
+    }
 
     public void ResetAndPopulateEnemyList()
     {
@@ -149,26 +115,5 @@ public class Wave : MonoBehaviour
             Debug.Log("Enemy list reset and populated with children.");
         }
     }
-
-
-    //private void OnDrawGizmos()
-    //{
-
-    //        Vector3 position = transform.position;
-    //        float size = 6.0f; // Set the size of your rectangle
-
-    //        // Define the corners of the rectangle
-    //        Vector3 corner1 = position + new Vector3(0, 50-size, -size);
-    //        Vector3 corner2 = position + new Vector3(0, 50 + size, -size);
-    //        Vector3 corner3 = position + new Vector3(0, 50 + size, size);
-    //        Vector3 corner4 = position + new Vector3(-0, 50 - size, size);
-
-    //        // Draw lines between the corners to form a rectangle
-    //        Gizmos.DrawLine(corner1, corner2);
-    //        Gizmos.DrawLine(corner2, corner3);
-    //        Gizmos.DrawLine(corner3, corner4);
-    //        Gizmos.DrawLine(corner4, corner1);
-    //    }
-
-    }
+}
 
