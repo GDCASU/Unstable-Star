1a08f0a260f8aa02da6df7bd5749925abc9024fc
Shantanu <95384818+ShantanuT26@users.noreply.github.com>
2024-04-12 16:21:30 -0700

    Created ScriptableObject script, and UICredScript
    committing because I am leaving club meet rn

---
+++ Assets/UICred.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UICred : MonoBehaviour
+{
+   // [SerializeField] private TMP_Text mytxt;
+   // CredText soText;
+   // [SerializeField] private GameObject mypanel;
+  //  float speed;
+    void Update()
+    {
+    }
+}
