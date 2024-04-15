f12e61023f17e4700f9715982798fdcbc9177d5e
Shantanu <95384818+ShantanuT26@users.noreply.github.com>
2024-04-14 19:55:18 -0700

    Panel moves up, and scene can switch
    Scene switches to menu when panel.y is above a certain position.

---
+++ Assets/CredText.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CredText : ScriptableObject
+{
+    public string text = "asdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+        "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+        "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+        "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+        "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+}
--- Assets/UICred.cs
+++ Assets/UICred.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.AssetImporters;
 using UnityEngine;
 
 public class UICred : MonoBehaviour
 {
-   // [SerializeField] private TMP_Text mytxt;
-   // CredText soText;
+    // [SerializeField] private TMP_Text mytxt;
+    [SerializeField]CredText soText;
+    [SerializeField] TMP_Text mytext;
    // [SerializeField] private GameObject mypanel;
-  //  float speed;
+    float speed = 2f;
+    Vector3 moving = new Vector3(0,1,0);
+    private void Start()
+    {
+        mytext.text = soText.text;
+    }
     void Update()
     {
+        this.gameObject.transform.Translate(moving*speed*Time.deltaTime);
+        if(this.gameObject.transform.position.y>0)
+        {
+            ScenesManager.instance.LoadScene(Scenes.MainMenu);
+        }
     }
 }
