62b95dfc8c156b5d128db6a89bf4556650035ba2
Ryan Carpenter <dev.ryan1184@gmail.com>
2024-04-17 19:06:30 -0700

    Gatling Gun Movement Theoretical

--- Assets/Scripts/Entity Related/Enemies/GatlingEnemy.cs
+++ Assets/Scripts/Entity Related/Enemies/GatlingEnemy.cs
@@ -7,13 +7,13 @@ public class GatlingEnemy : Enemy
 {
     [SerializeField] private GatlingCrosshair crosshair;
     [SerializeField] private GameObject Gun;
+    [SerializeField] private Vector3 worldUp = Vector3.up;
 
     private ShootComponent GatlingShootComp;
 
     protected override void Start()
     {
         base.Start();
-        Debug.Log("here");
         GatlingShootComp = GetComponent<ShootComponent>();
     }
 
@@ -25,10 +25,7 @@ public class GatlingEnemy : Enemy
 
     private void MoveGun()
     {
-        // Create a normalized vector between the gatling gun and the crosshair
-        Vector2 direction = Vector3.Normalize(crosshair.transform.position - transform.position);
-        Debug.Log(direction);
         // Rotate the gun
-        Gun.transform.LookAt(direction);
+        Gun.transform.LookAt(crosshair.transform.position, worldUp);
     }
 }
