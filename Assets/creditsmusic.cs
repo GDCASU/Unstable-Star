66ee6c22e4c3ffe4b7d81198578e6ed2d5bbfb13
EV401SALVATION <dmharp@asu.edu>
2024-04-19 15:02:28 -0700

    4/19/24 Finalized Credits
    Finalized credits.

---
+++ Assets/creditsmusic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creditsmusic : MonoBehaviour
+{
+    private void Start()
+    {
+        //SoundManager.instance.PlaySound(soundta)
+    }
+}
