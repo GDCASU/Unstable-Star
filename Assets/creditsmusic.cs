8d3e490735bdd1c6e2a42c968182265a4fca5121
Ryan Carpenter <79438364+dev-rygy@users.noreply.github.com>
2024-04-19 15:12:22 -0700

    ,jbkjb

--- Assets/creditsmusic.cs
+++ Assets/creditsmusic.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class creditsmusic : MonoBehaviour
+public class CreditsMusic : MonoBehaviour
 {
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        //SoundManager.instance.PlaySound(soundta)
+        SoundManager.instance.PlaySound(SoundLibrary.CreditsTrack);
     }
+
 }
