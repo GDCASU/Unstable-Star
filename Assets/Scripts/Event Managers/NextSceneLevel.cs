2669f0ba41ea4e225dada3aca0f38ddb580ebaca
Ananyaappl3 <145163387+Ananyaappl3@users.noreply.github.com>
2024-04-18 22:44:11 -0700

    Update NextSceneLevel singleton

--- Assets/Scripts/Event Managers/NextSceneLevel.cs
+++ Assets/Scripts/Event Managers/NextSceneLevel.cs
@@ -5,7 +5,8 @@ using UnityEngine.Events;
 
 public class NextSceneLevel : MonoBehaviour
 {
-    
+    //singleton reference
+    public static NextSceneLevel instance;
     
     // Start is called before the first frame update
     private void Start()
