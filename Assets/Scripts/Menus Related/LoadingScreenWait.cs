6c5f890b6d585bfe1b8e60085aa4db79d89698f7
Aaron Huggins <achuggi1@asu.edu>
2024-06-11 11:54:23 -0700

    Make loading screen look nice

--- Assets/Scripts/Menus Related/LoadingScreenWait.cs
+++ Assets/Scripts/Menus Related/LoadingScreenWait.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 /// </summary>
 public class LoadingScreenWait : MonoBehaviour
 {
+    [SerializeField] float forcedLoadingTime = 1f;
+    float stopwatch = 0f;
+
     void Start()
     {
         StartCoroutine(WaitForDataToLoad());
@@ -17,6 +20,12 @@ public class LoadingScreenWait : MonoBehaviour
     /// </summary>
     private IEnumerator WaitForDataToLoad()
     {
+        // Give time for player to look at graphics
+        while(stopwatch < forcedLoadingTime)
+        {
+            stopwatch += Time.deltaTime;
+            yield return null;
+        }
         // Wait for the singleton to be set
         while (SerializedDataManager.instance == null) yield return null;
         // Wait for the data to load
