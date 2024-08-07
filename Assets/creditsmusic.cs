dded95b9d874d0ac1bb7981c9a18ff8cbc8c2bd1
Aaron Huggins <achuggi1@asu.edu>
2024-08-06 19:49:40 -0700

    credits skip
    put the skip functionality into the credits scene.

--- Assets/creditsmusic.cs
+++ Assets/creditsmusic.cs
@@ -8,11 +8,19 @@ public class CreditsMusic : MonoBehaviour
     void Start()
     {
         SoundManager.instance.PlaySound(SoundLibrary.CreditsTrack);
+
+        PlayerInput.instance.ActivateUiControls();
+    }
+
+    private void OnDestroy()
+    {
+        PlayerInput.instance.ActivateShipControls();
     }
 
     public void StopMusic() 
     {
         SoundManager.instance.PauseAllSounds();
+        ScenesManager.instance.LoadScene(Scenes.MainMenu);
     }
 
 }
